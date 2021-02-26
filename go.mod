61cac69d538e6a8a2b122ad3385d650c6dcc5da1
mt5225 <mt5225@gmail.com>
2021-02-25 18:07:13 -0800

    update

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/mt5225/terraform-provider-gateway
 go 1.12
 
 require (
-  github.com/hashicorp/terraform >=0.13
-  github.com/mt5225/cloudj-gateway-go >=1.0.0
+	github.com/hashicorp/terraform v0.13.5
+	github.com/mt5225/cloudj-gateway-go v1.0.1
 )
--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
-	"github.com/mt5225/terraform-provider-cloudj-gateway/gateway"
+	"github.com/mt5225/terraform-provider-gateway/gateway"
 )
 
 func main() {
